7c5919a373c9da141d169859e1a6a9488fa0a988
s-mihara <pub.pyros@gmail.com>
2016-01-28 11:23:39 +0900

    sitemap修正

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>http://applemusictweet.pyroz.net</loc>
+  </url>
   @foreach ($results as $result)
   <url>
     <loc>http://applemusictweet.pyroz.net/detail/{{ rawurlencode($result->parent_title) }}</loc>
--- resources/views/sp_detail.blade.php
+++ resources/views/sp_detail.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AppleMusicTweet ｜ {{$parentTitle}}のツイート一覧</title>
+    <title>{{$parentTitle}}のAppleMusicツイート一覧 | AppleMusicTweet </title>
 
     <!-- Bootstrap -->
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
